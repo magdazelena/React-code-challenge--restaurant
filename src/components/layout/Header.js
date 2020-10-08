0ac2b4786866dc98329e3991ca80865fb4e959c0
magdazelena <magda.zelezik@gmail.com>
2020-10-08 10:24:27 +0200

    Setup project

---
+++ src/components/layout/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul class="main-menu">
+          <li>
+            <a href="/" aria-label="Back to front page" id="logo">
+              <img src="./images/logo.svg" alt="Sundown Boulevard logo" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              Restauranter
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              Produkter
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              Nyhedsbrev
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              Kontakt
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header;
---
+++ src/components/layout/Page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <div class="content-page">
+
+    </div>
+  )
+}
+export default Page;
