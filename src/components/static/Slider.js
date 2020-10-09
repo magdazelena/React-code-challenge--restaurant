9628b5f4e1735e4c09eb1d8089662f3d5a2a4bde
magdazelena <magda.zelezik@gmail.com>
2020-10-09 21:04:04 +0200

    Added a front page slider

--- src/components/static/Slider.js
+++ src/components/static/Slider.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 function Slider() {
-  return (<div className="slider">Slider</div>)
+  return (<div className="slider">
+    <Carousel autoPlay dynamicHeight={false} showThumbs={false} infiniteLoop>
+      <div>
+        <img src="./images/slides/table.jpg" />
+      </div>
+      <div>
+        <img src="./images/slides/meat.jpg" />
+      </div>
+      <div>
+        <img src="./images/slides/sushi.jpg" />
+      </div>
+      <div>
+        <img src="./images/slides/fries.jpg" />
+      </div>
+    </Carousel>
+  </div>)
 }
 export default Slider;
